3f82f2032ea6c00e45b02e9a39d620ef8075131a
LUIS <daemo159@gmail.com>
2022-02-24 12:02:17 -0500

    ADDED SHOPPING MENU BORDER AND STYLE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const cliente = {
-    nombre: 'server',
-    llamarCliente:
-
+function solicitudServidor(consulta,callback){
+    setTimeout(function() {
+        var respuesta = consulta + " esta lleno!"
+        callback(respuesta)
+    }, 15000);
+    
 }
+function answer(result){
+    console.log("respuesta del servidor: " + result)
+} 
+solicitudServidor("la peticion ",answer); 
