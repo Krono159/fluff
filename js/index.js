0f52bb325527e246b368d153acf3cc321c5d228e
LUIS <daemo159@gmail.com>
2022-03-02 11:46:43 -0500

    ignored js/index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-type':'text/html'});
-    res.write('<h1>puto el que lo lea</h1>');
-    res.end();
-}).listen(3000);
