059f2f987b664d4f3f3def75d448ff2f1bc10f53
LUIS <daemo159@gmail.com>
2022-03-02 11:41:52 -0500

    removing gallery-es.html and gallery-en.html
    removing gallery-es.html and gallery-en.html , added gallery folder with both languages. adding all the sections pending

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write('<h1>puto el que lo lea</h1>');
+    res.end();
+}).listen(3000);
